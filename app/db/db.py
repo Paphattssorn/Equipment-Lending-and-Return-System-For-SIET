fbafd0db05a75c4a7d548da1438ccf39c3f02474
Paphatsorn <paphattssorn@gmail.com>
2025-10-14 15:29:02 +0700

    update database-deploy

--- app/db/db.py
+++ app/db/db.py
@@ -1,10 +1,11 @@
-# app/db/db.py
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker, scoped_session
 
-
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///app.db")
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 connect_args = {}
 if DATABASE_URL.startswith("sqlite"):
